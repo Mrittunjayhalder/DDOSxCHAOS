6cf5ca1e974174f9db07c9a6c93719e6b641b5b0
FGod47 <118654823+FGod47@users.noreply.github.com>
2024-07-23 12:08:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6480583814:AAG4iqwsAopEP_BeKyUIsmUSXfd667Pgdiw")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("HelLluciFer47")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("861664662")
 
 
 
